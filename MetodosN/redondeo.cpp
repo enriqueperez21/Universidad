462c1ac54a7d2667ccde5b37515fe05c8c4735e5
enriqueperez21 <luis.perez05@epn.edu.ec>
2024-05-09 14:40:58 -0500

    Metodos numericos

---
+++ MetodosN/redondeo.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <cstdint>
+
+int main(){
+	uint8_t x = 300;
+	std::cout << x + 0 <<std::endl;
+	return 0;
+}
---
+++ MetodosN/redondeo.py
@@ -0,0 +1,3 @@
+import numpy as np
+result = np.uint8(300)
+print(result)

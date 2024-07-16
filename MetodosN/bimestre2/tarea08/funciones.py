47012dfefcb4a9aa6abeb3683391bc66377570f2
enriqueperez21 <luis.perez05@epn.edu.ec>
2024-07-16 08:57:45 -0500

    Metodos tarea 8 avance

--- MetodosN/bimestre2/tarea08/funciones.py
+++ MetodosN/bimestre2/tarea08/funciones.py
@@ -1,6 +1,7 @@
 import numpy as np
 import sympy as sp
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def calcular_exponencial(exponente):
     return np.exp(exponente)
@@ -11,15 +12,11 @@ def log_natural(x):
 def sumatoria(a):
     return np.sum(a)
 
-def grado2(df):
+def grado2(x, y):
     # Sumatorias necesarias
-    x_sum = sumatoria(df['x'])
-    y_sum = sumatoria(df['y'])
-    xy_sum = sumatoria(df['x'] * df['y'])
-    x2_sum = sumatoria(df['x'] ** 2)
-
+    x_sum , y_sum , xy_sum , x2_sum = sumatoria(x), sumatoria(y), sumatoria(x * y), sumatoria(x ** 2) 
     # Número de puntos
-    n = len(df)
+    n = len(x)
 
     # Definimos las incógnitas
     b0, b1 = sp.symbols('b0 b1')
@@ -30,20 +27,17 @@ def grado2(df):
 
     # Resolvemos el sistema de ecuaciones
     solution = sp.solve((eq1, eq2), (b0, b1))
-    return solution
+    b0,b1 = list(solution.values())
+    def my_function(x): return (b0 + x*b1)
+    return solution, my_function
 
-def grado3(df):
+def grado3(x, y):
     # Sumatorias necesarias
-    x_sum = sumatoria(df['x'])
-    y_sum = sumatoria(df['y'])
-    xy_sum = sumatoria(df['x'] * df['y'])
-    x2_sum = sumatoria(df['x'] ** 2)
-    x3_sum = sumatoria(df['x'] ** 3)
-    x4_sum = sumatoria(df['x'] ** 4)
-    x2y_sum = sumatoria(df['x'] ** 2 * df['y'])
+    x_sum , y_sum , xy_sum , x2_sum = sumatoria(x), sumatoria(y), sumatoria(x * y), sumatoria(x ** 2) 
+    x3_sum, x4_sum, x2y_sum = sumatoria(x ** 3), sumatoria(x ** 4), sumatoria(x ** 2 * y) 
 
     # Número de puntos
-    n = len(df)
+    n = len(x)
 
     # Definimos las incógnitas
     a, b, c = sp.symbols('a b c')
@@ -55,23 +49,18 @@ def grado3(df):
 
     # Resolvemos el sistema de ecuaciones
     solution = sp.solve((eq1, eq2, eq3), (a, b, c))
-    return solution
+    a,b,c = list(solution.values())
+    def my_function(x): return (a*x**2 + b*x + c)
+    return solution, my_function
 
-def grado4(df):
+def grado4(x, y):
     # Sumatorias necesarias
-    x_sum = sumatoria(df['x'])
-    y_sum = sumatoria(df['y'])
-    xy_sum = sumatoria(df['x'] * df['y'])
-    x2_sum = sumatoria(df['x'] ** 2)
-    x3_sum = sumatoria(df['x'] ** 3)
-    x4_sum = sumatoria(df['x'] ** 4)
-    x5_sum = sumatoria(df['x'] ** 5)
-    x6_sum = sumatoria(df['x'] ** 6)
-    x2y_sum = sumatoria(df['x'] ** 2 * df['y'])
-    x3y_sum = sumatoria(df['x'] ** 3 * df['y'])
+    x_sum , y_sum , xy_sum , x2_sum = sumatoria(x), sumatoria(y), sumatoria(x * y), sumatoria(x ** 2) 
+    x3_sum, x4_sum, x2y_sum = sumatoria(x ** 3), sumatoria(x ** 4), sumatoria(x ** 2 * y) 
+    x5_sum, x6_sum, x3y_sum = sumatoria(x ** 5), sumatoria(x ** 6), sumatoria(x ** 3 * y)
 
     # Número de puntos
-    n = len(df)
+    n = len(x)
 
     # Definimos las incógnitas
     a, b, c, d = sp.symbols('a b c d')
@@ -84,18 +73,16 @@ def grado4(df):
 
     # Resolvemos el sistema de ecuaciones
     solution = sp.solve((eq1, eq2, eq3, eq4), (a, b, c, d))
-    return solution
+    a,b,c, d = list(solution.values())
+    def my_function(x): return (a*x**3 + b*x**2 + c*x + d)
+    return solution, my_function
 
-def resolver_d(df):
-    x = (df['x'])
-    x_sum = sumatoria(x)
-    x2_sum = sumatoria(x**2)
-    y = (df['y'])
-    Y_log = log_natural(df['y'])
-    Y_log_sum = sumatoria(Y_log)
-    XY_log_sum = sumatoria(x*Y_log)
+def resolver_d(x, y):
+    x_sum, x2_sum = sumatoria(x), sumatoria(x**2)
+    Y_log = log_natural(y)
+    Y_log_sum, XY_log_sum = sumatoria(Y_log), sumatoria(x*Y_log)
     # Número de puntos
-    n = len(df)
+    n = len(x)
 
     # Definimos las incógnitas
     B_log, a = sp.symbols('B_log a')
@@ -110,4 +97,17 @@ def resolver_d(df):
     b = float(a[0])
     a = float(a[1])
     b = np.exp(b)
-    return {'a': a,'b':b}
+    def my_function(x): return (b*np.exp(a*x))
+    return {'a': a,'b':b}, my_function
+
+def plot_points_line(x, y, x_graph, y_graph):
+    fig, ax = plt.subplots(figsize=(6,4))
+    ax.scatter(x, y, label='Datos originales')
+    ax.plot(x_graph, y_graph, color='red', label='Datos predecidos')
+    ax.legend()
+
+def plot_points(x, y):
+    fig, ax = plt.subplots(figsize=(6,4))
+    ax.scatter(x, y, label='Datos originales')
+    ax.legend()
+    pass

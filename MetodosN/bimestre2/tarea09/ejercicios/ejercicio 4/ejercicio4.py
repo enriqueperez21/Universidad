155523957dcaf90575aadfe6dc33c53b1ebe55ff
Luis Enrique Pérez Señalin <luis.perez05@epn.edu.ec>
2024-08-12 21:14:17 -0500

    Métodos N tarea09

---
+++ MetodosN/bimestre2/tarea09/ejercicios/ejercicio 4/ejercicio4.py
@@ -0,0 +1,65 @@
+import numpy as np
+
+def eliminacion_gaussiana_32bits(A: np.ndarray) -> np.ndarray:
+    """Resuelve un sistema de ecuaciones lineales mediante el método de eliminación gaussiana usando precisión de 32 bits.
+
+    ## Parameters
+
+    ``A``: matriz aumentada del sistema de ecuaciones lineales. Debe ser de tamaño n-by-(n+1), donde n es el número de incógnitas.
+
+    ## Return
+
+    ``solucion``: vector con la solución del sistema de ecuaciones lineales.
+
+    """
+    # Convertir A a tipo float32 para asegurar precisión de 32 bits
+    A = A.astype(np.float32)
+    
+    n = A.shape[0]
+
+    for i in range(0, n - 1):  # loop por columna
+
+        # --- encontrar pivote
+        p = None  # default, first element
+        for pi in range(i, n):
+            if A[pi, i] == 0:
+                # must be nonzero
+                continue
+
+            if p is None:
+                # first nonzero element
+                p = pi
+                continue
+
+            if abs(A[pi, i]) < abs(A[p, i]):
+                p = pi
+
+        if p is None:
+            # no pivot found.
+            raise ValueError("No existe solución única.")
+
+        if p != i:
+            # swap rows
+            _aux = A[i, :].copy()
+            A[i, :] = A[p, :].copy()
+            A[p, :] = _aux
+
+        # --- Eliminación: loop por fila
+        for j in range(i + 1, n):
+            m = A[j, i] / A[i, i]
+            A[j, i:] = A[j, i:] - m * A[i, i:]
+
+    if A[n - 1, n - 1] == 0:
+        raise ValueError("No existe solución única.")
+
+    # --- Sustitución hacia atrás
+    solucion = np.zeros(n, dtype=np.float32)
+    solucion[n - 1] = A[n - 1, n] / A[n - 1, n - 1]
+
+    for i in range(n - 2, -1, -1):
+        suma = 0
+        for j in range(i + 1, n):
+            suma += A[i, j] * solucion[j]
+        solucion[i] = (A[i, n] - suma) / A[i, i]
+
+    return solucion
